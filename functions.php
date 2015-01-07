dedd38d7daee1bed96f9a4fe8d446e4009eadf77
tonychiang <chiangloveslynn@gmail.com>
2015-01-07 22:04:06 +0800

    modify block sizes.

--- functions.php
+++ functions.php
@@ -257,12 +257,15 @@ twentythirteen theme where we can declare some
 external fonts. If you're using Google Fonts, you
 can replace these fonts, change it in your scss files
 and be up and running in seconds.
-*/
-function bones_fonts() {
-  wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
-}
 
-add_action('wp_enqueue_scripts', 'bones_fonts');
+# if need Google Web Fonts, Use the function below.
+*/
+
+// function bones_fonts() {
+//   wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+// }
+
+// add_action('wp_enqueue_scripts', 'bones_fonts');
 
 
 
